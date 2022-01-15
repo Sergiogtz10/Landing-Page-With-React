dd85e5f5faad73657589496bcf2164c4c9d9e54f
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2022-01-15 17:45:28 +0000

    feat: changes in navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,35 +2,54 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top bg-dark">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="#">
-					Start Bootstrap
-				</a>
-				<div
-					class="collapse navbar-collapse d-flex flex-row-reverse"
-					id="navbarNav">
-					<ul class="navbar-nav">
+				<div className="col-9">
+					<a class="navbar-brand text-white" href="#">
+						Start Bootstrap
+					</a>
+				</div>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarScroll"
+					aria-controls="navbarScroll"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarScroll">
+					<ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
 						<li class="nav-item">
 							<a
-								class="nav-link active"
+								class="nav-link active text-white"
 								aria-current="page"
 								href="#">
 								Home
 							</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
+						<li class="nav-item  ">
+							<a class="nav-link text-white" href="#">
 								About
 							</a>
 						</li>
+
 						<li class="nav-item">
-							<a class="nav-link" href="#">
+							<a
+								class="nav-link disabled text-white"
+								href="#"
+								tabindex="-1"
+								aria-disabled="true">
 								Services
 							</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">
+							<a
+								class="nav-link disabled text-white"
+								href="#"
+								tabindex="-1"
+								aria-disabled="true">
 								Contact
 							</a>
 						</li>
